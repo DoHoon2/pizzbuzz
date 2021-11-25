eb6f021fc218aecefb482eb6a894e8ec09bbe664
dohuen304 <dohuen304@naver.com>
2021-11-25 15:22:16 +0900

    docs: Add done to show it's done

--- pizzbuzz.py
+++ pizzbuzz.py
@@ -3,8 +3,8 @@ def do_fizzbuzz():
     Do fizzbuzz with for and if
     While i goes up 1 to 15,
     print 'fizz' if i is times of 3(done)
-    print 'buzz' if i is times of 5
-    print 'fizzbuzz' if i is times of 15
+    print 'buzz' if i is times of 5(done)
+    print 'fizzbuzz' if i is times of 15(done)
     else, print i (string)
     """
     for i in range(1,15+1):
